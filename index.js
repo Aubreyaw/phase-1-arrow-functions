5b42e4efadb7b0fe3ebe5dd862aa201657d8d1a7
Aubrey Whitford <Aubreya.whitford@gmail.com>
2024-02-17 17:00:46 -0500

    finishes lab

---
+++ index.js
@@ -0,0 +1,15 @@
+const divide = function(parameter1, parameter2){
+    return parameter1 / parameter2;
+}
+
+divide(2000,200)
+
+const square = (num) => num * num;
+
+square(20)
+
+const add = (num1, num2) => {
+    return (num1 + num2);
+}
+
+add(20,23)
